7657a7c880b8a0607e5929f0c55d661abe3f13e4
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-12-19 23:04:01 +0530

    Add files via upload
    Databinding, Components, Local references, Life cycle hooks.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { serverComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 @NgModule({
   declarations: [
     AppComponent,
-    serverComponent,
-    ServersComponent
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
